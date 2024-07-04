3317d1fc1293e16a0dd65bb19c5ed77a78044e24
JaeHyun <vv501610@gmail.comm>
2024-07-04 21:33:52 +0900

    로그인, 회원가입 기능 추가, 자신의 일정만 가져올수 있도록 변경, 로그아웃 버튼 디자인 변경, 현재 로그인한 유저 ID화면에 보이도록 표시

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -7,6 +8,7 @@ class Event(db.Model):
     title = db.Column(db.String(100), nullable=False)
     start = db.Column(db.String(100), nullable=False)
     end = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def to_dict(self):
         return {
@@ -15,3 +17,9 @@ class Event(db.Model):
             'start': self.start,
             'end': self.end
         }
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    events = db.relationship('Event', backref='user', lazy=True)
